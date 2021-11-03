c50d44e3a7b9977fcd7473d16c8cebb4a18a382f
Owali Ullah Shawon <33037708+OwaliShawon@users.noreply.github.com>
2021-11-03 17:53:27 +0600

    show movies page added with button

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, ImageBackground, StyleSheet, Text, View } from 'react-native';
+
+const image = { uri: "https://cdn.wallpapersafari.com/24/74/zgeTuV.jpg" };
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
+        {/* <Text style={styles.text}>Inside</Text> */}
+        <Button style={styles.button}
+        color="red"
+        title="SHOW MOVIES"
+        onPress={() => Alert.alert('Simple Button pressed')}
+      />
+      </ImageBackground>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +23,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // backgroundColor: '#fff',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+    justifyContent: "center"
+  },
+  button: {
+    backgroundColor: '#fff'
+  }
 });
